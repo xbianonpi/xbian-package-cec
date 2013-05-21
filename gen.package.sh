3696e64210fd6ed076bf167c4da9056424fe4102
Matus Kral <matuskral@me.com>
2013-05-21 19:27:30 +0200

    no files have been actually installed with previous packages.

--- gen.package.sh
+++ gen.package.sh
@@ -3,5 +3,7 @@
 package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
 
-fakeroot find ./content  | grep -v DEBIAN/ | xargs md5sum > ./content/DEBIAN/md5sums 
+cd content
+find ./ -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | sort | xargs md5sum > DEBIAN/md5sums
+cd ..
 fakeroot dpkg-deb -b ./content "${package}""${version}".deb
