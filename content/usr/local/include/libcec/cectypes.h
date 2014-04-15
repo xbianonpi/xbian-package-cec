b41e0c810839b505d4e323d1f8b5b12a1a735279
Matus Kral <matuskral@me.com>
2014-04-15 02:28:59 +0200

    latest libCEC for RPI with fixed POLLing during LA selection

--- content/usr/local/include/libcec/cectypes.h
+++ content/usr/local/include/libcec/cectypes.h
@@ -295,6 +295,17 @@ namespace CEC {
 #define CEC_TDA995x_VIRTUAL_COM		"CuBox"
 
 /*!
+ * the path to use for the i.MX CEC wire
+ */
+#define CEC_IMX_PATH		"/dev/mxc_hdmi_cec"
+
+/*!
+ * the name of the virtual COM port to use for the i.MX CEC wire
+ */
+#define CEC_IMX_VIRTUAL_COM		"i.MX"
+
+
+/*!
  * Mimimum client version
  */
 #define CEC_MIN_LIB_VERSION          2
@@ -858,7 +869,8 @@ typedef enum cec_adapter_type
   ADAPTERTYPE_P8_EXTERNAL      = 0x1,
   ADAPTERTYPE_P8_DAUGHTERBOARD = 0x2,
   ADAPTERTYPE_RPI              = 0x100,
-  ADAPTERTYPE_TDA995x          = 0x200
+  ADAPTERTYPE_TDA995x          = 0x200,
+  ADAPTERTYPE_IMX	       = 0x300,
 } cec_adapter_type;
 
 typedef struct cec_menu_language
